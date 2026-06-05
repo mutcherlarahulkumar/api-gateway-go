7b12b761dd734fdd5f2975b15b2b57ed31e43a94
Rahul kumar <rahulkumarmutcherla@gmail.com>
2026-06-05 23:34:21 +0530

    feat: add ratelimitter logic

--- middleware/rateLimiter.go
+++ middleware/rateLimiter.go
@@ -1,7 +1,39 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	RequestsPerMinute = 1
+)
 
 func RateLimiterMiddleware(c *gin.Context) {
+	ctx := c.Request.Context()
+	clientIP := c.ClientIP()
+
+	count, err := rdb.Incr(ctx, "rate:"+clientIP).Result()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, map[string]string{
+			"message": "Something went wrong!",
+		})
+
+		return
+	}
+
+	if count == 1 {
+		rdb.Expire(ctx, "rate:"+clientIP, time.Minute)
+	} else if count > RequestsPerMinute {
+		c.JSON(http.StatusTooManyRequests, map[string]string{
+			"message": "Rate limit exceeded!",
+		})
+
+		return
+	}
+
+	c.Next()
 
 }
