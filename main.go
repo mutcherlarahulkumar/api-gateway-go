a5d54056022414d5ea0f470ce959a49d0ad7289f
Rahul kumar <rahulkumarmutcherla@gmail.com>
2026-06-05 22:53:19 +0530

    feat: add basic api-gateway

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+var serviceMap = map[string]string{
+	"user-service":    "http://localhost:8081",
+	"product-service": "http://localhost:8082",
+}
+
+func main() {
+	router := gin.Default()
+
+	router.Any("/*path", GateWayHandler)
+
+	router.Run(":8080")
+}
+
+func GateWayHandler(c *gin.Context) {
+
+	parts := strings.Split(c.Request.RequestURI, "/")
+
+	servicePrefix := parts[1]
+
+	baseURL, exists := serviceMap[servicePrefix]
+	if !exists {
+		c.JSON(http.StatusNotFound, map[string]string{
+			"message": "Invalid Route",
+		})
+		return
+	}
+
+	var finalParts []string
+
+	for i := 2; i < len(parts); i++ {
+		finalParts = append(finalParts, parts[i])
+	}
+
+	finalURL := strings.Join(finalParts, "/")
+
+	c.Request.URL.Path = "/" + finalURL
+	targetURL, _ := url.Parse(baseURL)
+	proxy := httputil.NewSingleHostReverseProxy(targetURL)
+	proxy.ServeHTTP(c.Writer, c.Request)
+
+}
